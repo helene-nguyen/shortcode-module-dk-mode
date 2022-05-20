1207264346b6a091fd80d7f3fbce5089af840135
Yumicode <75518128+helene-nguyen@users.noreply.github.com>
2022-05-20 21:36:53 +0200

    [feat]add features

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ const app = {
     //~INITIALIZATION
     init() {
         console.log('Everything is working');
-        
+
         app.handleButtonClick();
     },
     //~METHODS
@@ -17,22 +17,44 @@ const app = {
         event.preventDefault();
 
         app.body.classList.toggle('dark-mode');
-        
+
         const formElement = document.getElementById('form');
         formElement.setAttribute('data-form-info', `${app.buttonClickCount++}`);
         //Controle
-        app.displayInputText(app.buttonClickCount);
+        app.displayInputText();
+        app.displayInputCount(app.buttonClickCount);
 
     },
-    displayInputColor() {
-    },
-    displayInputText(count) {
+  
+    displayInputText() {
         //add as value data-form-info
-        const inputText = document.querySelector('.text');
-        inputText.setAttribute('type', 'text');
-        inputText.setAttribute('placeholder', `${count}`)
-        
-    }
+        const bodyElement = app.body.classList.contains('dark-mode');
+        const inputsText = document.querySelectorAll('.text');
+
+        if (bodyElement) {
+            for (const inputText of inputsText) {
+                inputText.setAttribute('type', 'text');
+            }
+        } else {
+            for (const inputText of inputsText) {
+                inputText.setAttribute('type', 'hidden');
+            }
+        }
+    },
+    displayInputCount(count) {
+
+        const inputCounterElement = document.querySelector('.counter');
+        const bodyElement = app.body.classList.contains('dark-mode');
+
+        if (bodyElement) {
+            inputCounterElement.setAttribute('type', 'text');
+        } else {
+            inputCounterElement.setAttribute('type', 'hidden');
+        }
+ 
+        inputCounterElement.setAttribute('placeholder', `${count}`);
+    },
+    displayInputColor() { },
 
 
 };
